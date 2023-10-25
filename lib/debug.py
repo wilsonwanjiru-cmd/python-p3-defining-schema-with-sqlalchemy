6a02fc1c77ef7aab74e023d7795cfba24f848913
Wilson Wanjiru <wilsonmuita41@gmail.com>
2023-10-25 12:35:10 +0300

    Defining a Schema with SQLAlchemy ORM

--- lib/debug.py
+++ lib/debug.py
@@ -1,10 +1,6 @@
-#!/usr/bin/env python3
-
 from sqlalchemy import create_engine
 
-from sqlalchemy_sandbox import Student
-
-engine = create_engine('sqlite:///students.db')
-
 if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+    engine = create_engine('sqlite:///students.db')
+    Base.metadata.create_all(engine)
+
--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,12 +1,21 @@
-#!/usr/bin/env python3
-
-from sqlalchemy import Column, Integer, String, create_engine
+# Import necessary modules
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
 
+# Define a declarative base
 Base = declarative_base()
 
+# Define a class for the 'students' table
 class Student(Base):
-    pass
+    __tablename__ = 'students'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String())
 
 if __name__ == '__main__':
-    pass
+    # Create an SQLite database engine
+    engine = create_engine('sqlite:///students.db')
+
+    # Create the 'students' table in the database
+    Base.metadata.create_all(engine)
